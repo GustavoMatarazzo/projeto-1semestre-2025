935d2fcfbb3042057b21523da59fede9730c6d1b
GustavoMatarazzo <gm29176@gmail.com>
2025-02-27 22:25:29 -0300

    aula 2702

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ git config -–global user.name “seu nome”
 
 git config -–global user.email “seu email”
  
+spring.devtools.restart.enabled=true
+
+
--- src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
@@ -9,7 +9,34 @@ public class exercicioController {
     
     @GetMapping("{nome}")
     public String HelloWorld(@PathVariable String nome){
-        return "NOME"; 
+        return "NOME2"; 
 
     }
+
+    @GetMapping("/get-idade/{idade}")
+    public String RetornaIdade(@PathVariable Integer idade) {
+        try {
+            if (idade < 0 ) {
+                throw new NumberFormatException();
+            }
+
+            if (idade < 12 ) {
+                return "Criança";
+            } else if (idade <=18 ){
+                return "Adolescente"; 
+            } else if (idade <=60 ) {
+                return "Adulto";
+            } else {
+                return "idoso"; 
+            }
+
+        } catch (NumberFormatException e) {
+            return "idade inválida";
+        }
+    }
+
+    @GetMapping("/get-par-impar/{numero}")
+    public String RetornaParImpar(@PathVariable Integer numero){
+    }
+
 }
