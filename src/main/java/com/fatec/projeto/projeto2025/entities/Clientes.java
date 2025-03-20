cf5b76e5da4aa274f524e585e17d9294183b3910
GustavoMatarazzo <gm29176@gmail.com>
2025-03-19 23:00:48 -0300

    Update Clientes.java

--- src/main/java/com/fatec/projeto/projeto2025/entities/Clientes.java
+++ src/main/java/com/fatec/projeto/projeto2025/entities/Clientes.java
@@ -1,30 +1,57 @@
-package com.fatec.projeto.projeto2025;
 
-public class Clientes {
-    private int id;
+package com.fatec.projeto.projeto2025.entities;
+
+
+public class Cliente {
+    private Long id;
     private String nome;
-    private int idade;
-    //private ;
+    private Integer idade;
+    private String endereco;
 
-    public Clientes() {
+    // Construtores
+    public Cliente() {}
+
+    public Cliente(Long id, String nome, int idade, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
     }
 
-    public int getId() {
+    // Getters e Setters
+    public Long getId() {
         return id;
     }
-    public void setId(int id) {
+
+    public void setId(Long id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public int getIdade() {
+
+    public Integer getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
     }
 }
